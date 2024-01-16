f9d87ee9999ef3ad05fd28003807d5f064352b97
Shashank Rajput <hariomshashank@gmail.com>
2024-01-16 23:18:30 +0530

    uploading to git

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import WeatherApp from "./WeatherApp";
+import React from "react";
+
+function App() {
+  return (
+    <>
+      <WeatherApp />
+    </>
+  );
+}
+
+export default App
---
+++ src/WeatherApp.jsx
@@ -0,0 +1,25 @@
+import SearchBox from "./searchBox";
+import InfoBox from "./InfoBox";
+import {useState } from "react";
+
+export default function WeatherApp () {
+    const [weatherInfo , setWeatherInfo] = useState({
+        city : "Delhi",
+        feelsLike : 16.35,
+        humidity : 59,
+        temp : 17.05,
+        tempMax : 17.05,
+        tempMin : 17.05,
+        weather : "smoke",
+    });
+    let updateInfo = (newInfo) => {
+        setWeatherInfo(newInfo);
+    }
+    return (
+        <div style={ {textAlign:"center"}}>
+            <h2 style={{fontFamily:"sans-serif"}}>Weather web-app by - SHASHANK RAJPUT</h2>
+            <SearchBox updateInfo={updateInfo}/>
+            <InfoBox info={weatherInfo}/>
+        </div>
+    )
+}
